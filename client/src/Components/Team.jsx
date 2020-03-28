3d589f76400824e005c66791ba5731c35c2c2a3c
Ipsita Rao <ipsita.rao@gmail.com>
2020-03-28 18:02:41 -0400

    description typo fix

--- client/src/Components/Team.jsx
+++ client/src/Components/Team.jsx
@@ -8,7 +8,7 @@ const Team = () => {
         <div className = "team-container" id = "team">
             <h2>The Team</h2>
             <div className = "abt-nishi">
-            <div className="nishibio">Hi. I'm Nishitha Khambaladinne and I'm amazing. You should work with me because I'll find the best palce for you. I am the best real estate agent in the NJ, NY, PA region. You won't reget working with me because 95% of the time I'm right</div>
+            <div className="nishibio">Hi. I'm Nishitha Khambaladinne and I'm amazing. You should work with me because I'll find the best place for you. I am the best real estate agent in the NJ, NY, PA region. You won't reget working with me because 95% of the time I'm right.</div>
             <div className = "nishipic"></div>
             </div>
         
